ad12b08809fdc808f8d038e0839fe4f12a5e3431
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-17 07:10:22 -0400

    use debug, merge answers

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var extend = require('extend-shallow');
+var debug = require('debug')('ask-when');
+var merge = require('mixin-deep');
 var isValid = require('is-valid-app');
 var isAnswer = require('is-answer');
 var get = require('get-value');
 
 function askWhen(app, name, options, cb) {
+  debug('initializing from <%s>', __filename);
+
   if (typeof options === 'function') {
     cb = options;
     options = {};
@@ -15,13 +18,21 @@ function askWhen(app, name, options, cb) {
     throw new TypeError('expected a callback function');
   }
 
-  var opts = extend({save: false}, app.options, options);
-  var val = get(opts, name) || get(opts.data, name);
+  var opts = merge({save: false}, app.base.options, app.options, options);
+  var skip = get(opts, ['question.skip', name].join('.'));
+  var data = merge({}, app.cache.data, opts.data, app.cache.answers);
+  var val = get(data, name) || get(opts, name);
+
   var answers = {};
   answers[name] = val;
 
   var isAnswered = isAnswer(val);
   opts.force = isAnswered !== true;
+
+  if (isAnswered || skip === true) {
+    opts.askWhen = 'not-answered';
+    opts.force = false;
+  }
 
   // conditionally prompt the user
   switch (opts.askWhen) {
